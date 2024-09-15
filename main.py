b2c044b3c9d2ee61d7e30c4060a25dd9c8d09674
justArale <just1arale@gmail.com>
2024-09-15 14:40:17 +0200

    add Berlin as default city and add code-comments

--- main.py
+++ main.py
@@ -3,14 +3,21 @@ from utils import save_forecast_as_html
 from email_sender import send_forecast_email
 
 def main():
-    location = input("Enter your location: ")
+    # Type the location or leave empty and use Berlin by default
+    location = input("Enter your location (press Enter for Berlin by default): ")
+    if not location:
+        location = 'Berlin'
+
+    # Enter the recipient's email address
     mail_to= input ("Enter your Email: ")
+
+    # Start the function to fetch the weather forecast for the specified location
     forecast = get_weather_forecast(location)
     
     # Save/Display data in 3 different ways
-    save_forecast_as_html(forecast)
-    print(forecast)
-    send_forecast_email(forecast, location, mail_to)
+    save_forecast_as_html(forecast) # Save forecast in an HTML-file
+    print(forecast) # Print forecast in the console
+    send_forecast_email(forecast, location, mail_to) # Send forecast as an e-mail
 
 if __name__ == "__main__":
     main()
