0b2c15f07b6432f7c2467a1144d87decb86bbc4e
justArale <just1arale@gmail.com>
2024-09-16 11:48:03 +0200

    save the temepratur chart in the html, also restructure the html #2

--- utils.py
+++ utils.py
@@ -1,4 +1,16 @@
 # Helper function to save the weather data in forecast.html
-def save_forecast_as_html(forecast, location):
+def save_forecast_as_html(forecast, location, temperatur_chart):
     with open('forecast.html', 'w') as file:
-        file.write(f'14-Tage Wettervorhersage für {location} \n {forecast}')
+        file.write(f'''
+        <html>
+        <head>
+            <title>14-Day Weather Forecast for {location}</title>
+        </head>
+        <body>
+            <h1>14-Day Weather Forecast for {location}</h1>
+            <pre>{forecast}</pre>
+            <h2>Temperature Chart</h2>
+            <img src="{temperatur_chart}" alt="Temperature Chart">
+        </body>
+        </html>
+        ''')
